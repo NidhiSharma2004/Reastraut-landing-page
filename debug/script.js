913ecfbd5c0c7a4fa0c93fdeb0f8173c693f7c47
NidhiSharma2004 <vidhi6643@gmail.com>
2022-01-13 13:52:33 +0530

    debug and add button dynamically

--- debug/script.js
+++ debug/script.js
@@ -62,105 +62,61 @@ const menu = [
         title:"momos",
         price:"$53",
         decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+    },
+    {
+        image:"images/momo.jpg",
+        id:8,
+        category:"diner",
+        title:"momos",
+        price:"$53",
+        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+    },
+    {
+        image:"images/momo.jpg",
+        id:8,
+        category:"shake",
+        title:"momos",
+        price:"$53",
+        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     }
     
 ]
-let sectionCentre = document.querySelector(".section-centre");
-let btns = document.querySelectorAll(".btns");
-let filterbtn = document.querySelector(".filterbtn")
 
-// load the item
+let filterbtn=document.querySelector(".filterbtn")
 window.addEventListener("DOMContentLoaded",function(){
-    displayItemMenu(menu);
     displayButton(menu);
 })
 
 
-// BUTTON AND FILTERING SECTION
 
 function displayButton(menu){
-
     // get all category
     let allCategory = menu.map(item =>{
+
         return item.category
-    })
+    }) 
+    console.log(allCategory);
 
     // make all category in order
     allCategory.sort();
-
     // make an empty array to get all unique category
-    let oneCategory = ["all"];
+    let oneCategory = ["ALL"];
     for(let i=0;i<allCategory.length;i++){
         if(allCategory[i]!=allCategory[i+1]){
             oneCategory.push(allCategory[i])
         }
     }
-    
+    console.log(oneCategory);
+    console.log(oneCategory.length);
+    console.log(oneCategory[3])
     // to show btn
+    
     for(let i=0;i<oneCategory.length;i++){
         filterbtn.innerHTML +=  `<button data-id="${oneCategory[i]}" class=" btns">${oneCategory[i]}</button>`
-        
-        // GET THE CLASS OF ALL BUTTON AND USE FOREACH METHOD
         let btns = document.querySelectorAll(".btns")
-        btns.forEach(btn=>{
-            btn.addEventListener("click",(e)=>{ 
-                let category = e.currentTarget.dataset.id;    
-                // console.log(category)
-              const CategoryMenu = menu.filter(item=>{
-                if(category==item.category){
-                    return item;
-                }
-              })
-              // if category is equal to all then display all item
-              if(category=="all"){
-                displayItemMenu(menu)
-              }
-              // if category is not equal to all then show the filter arry
-              else{
-                displayItemMenu(CategoryMenu)
-              }
-            })
-        })
+       
     }
 }
 
 
 
-
-
-
-
-// display the item
-function displayItemMenu(menuItem){
-    let displayMenu = menuItem.map(menu =>{
-    return `
-    <div class="items">
-    <div class="image">
-        <img src=${menu.image}>
-    </div>
-    <div class="info">
-    <div class="title">
-        ${menu.title}
-    </div>
-    <div class="price">
-        ${menu.price}
-    </div>
-    </div>
-    <div class="desc">
-     <p>${menu.decs}</p>
-    </div>
-</div>
-    `
-    });
-    // console.log(displayItem)
-    displayMenu=displayMenu.join("")
-    sectionCentre.innerHTML= displayMenu
-}
-// scrol window on click                         
-function scroll(){
-    window.scrollBy({
-        top:1000,
-        behavior:"smooth"
-    })
-}
-document.querySelector(".filterbtn").addEventListener("click",scroll)
