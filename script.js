eecdac0aa8584b9fc71e75cd7b1a842920e3c404
NidhiSharma2004 <vidhi6643@gmail.com>
2022-01-13 14:05:40 +0530

    cart btn added

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
 const menu = [
     {
-        image:"images/burger.jpg",
-        id:1,
-        category:"fast food",
-        title:"Burger",
-        price:"$43",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+        image: "images/burger.jpg",
+        id: 1,
+        category: "fast food",
+        title: "Burger",
+        price: "$43",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     },
     {
-        image:"images/chilli.jpg",
-        id:2,
-        category:"fast food",
-        title:"chilli patato",
-        price:"$34",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
-    },
-    {   
-        image:"images/chowmin.jpg",
-        id:3,
-        category:"fast food",
-        title:"chowmin",
-        price:"$40",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+        image: "images/chilli.jpg",
+        id: 2,
+        category: "fast food",
+        title: "chilli patato",
+        price: "$34",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     },
     {
-        image:"images/pasta2.jpg",
-        id:4,
-        category:"breakfast",
-        title:"pasta",
-        price:"$50",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+        image: "images/chowmin.jpg",
+        id: 3,
+        category: "fast food",
+        title: "chowmin",
+        price: "$40",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     },
     {
-        image:"images/paner.jpg",
-        id:5,
-        category:"breakfast",
-        title:"paneer tika",
-        price:"$60",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+        image: "images/pasta2.jpg",
+        id: 4,
+        category: "breakfast",
+        title: "pasta",
+        price: "$50",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     },
     {
-        image:"images/mango.jpg",
-        id:6,
-        category:"breakfast",
-        title:"mango shake",
-        price:"$43",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+        image: "images/paner.jpg",
+        id: 5,
+        category: "breakfast",
+        title: "paneer tika",
+        price: "$60",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     },
     {
-        image:"images/salad.jpg",
-        id:7,
-        category:"breakfast",
-        title:"salad",
-        price:"$43",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+        image: "images/mango.jpg",
+        id: 6,
+        category: "breakfast",
+        title: "mango shake",
+        price: "$43",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     },
     {
-        image:"images/momo.jpg",
-        id:8,
-        category:"breakfast",
-        title:"momos",
-        price:"$53",
-        decs:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+        image: "images/salad.jpg",
+        id: 7,
+        category: "breakfast",
+        title: "salad",
+        price: "$43",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
+    },
+    {
+        image: "images/momo.jpg",
+        id: 8,
+        category: "breakfast",
+        title: "momos",
+        price: "$53",
+        decs: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis ipsum praesentium provident nisi odit distinctio ipsam delectus soluta cum possimus"
     }
-    
+
 ]
 let sectionCentre = document.querySelector(".section-centre");
 let btns = document.querySelectorAll(".btns");
 let filterbtn = document.querySelector(".filterbtn")
 
 // load the item
-window.addEventListener("DOMContentLoaded",function(){
+window.addEventListener("DOMContentLoaded", function () {
     displayItemMenu(menu);
     displayButton(menu);
 })
@@ -78,10 +78,10 @@ window.addEventListener("DOMContentLoaded",function(){
 
 // BUTTON AND FILTERING SECTION
 
-function displayButton(menu){
+function displayButton(menu) {
 
     // get all category
-    let allCategory = menu.map(item =>{
+    let allCategory = menu.map(item => {
         return item.category
     })
 
@@ -90,35 +90,35 @@ function displayButton(menu){
 
     // make an empty array to get all unique category
     let oneCategory = ["all"];
-    for(let i=0;i<allCategory.length;i++){
-        if(allCategory[i]!=allCategory[i+1]){
+    for (let i = 0; i < allCategory.length; i++) {
+        if (allCategory[i] != allCategory[i + 1]) {
             oneCategory.push(allCategory[i])
         }
     }
-    
+
     // to show btn
-    for(let i=0;i<oneCategory.length;i++){
-        filterbtn.innerHTML +=  `<button data-id="${oneCategory[i]}" class=" btns">${oneCategory[i]}</button>`
-        
+    for (let i = 0; i < oneCategory.length; i++) {
+        filterbtn.innerHTML += `<button data-id="${oneCategory[i]}" class=" btns">${oneCategory[i]}</button>`
+
         // GET THE CLASS OF ALL BUTTON AND USE FOREACH METHOD
         let btns = document.querySelectorAll(".btns")
-        btns.forEach(btn=>{
-            btn.addEventListener("click",(e)=>{ 
-                let category = e.currentTarget.dataset.id;    
+        btns.forEach(btn => {
+            btn.addEventListener("click", (e) => {
+                let category = e.currentTarget.dataset.id;
                 // console.log(category)
-              const CategoryMenu = menu.filter(item=>{
-                if(category==item.category){
-                    return item;
+                const CategoryMenu = menu.filter(item => {
+                    if (category == item.category) {
+                        return item;
+                    }
+                })
+                // if category is equal to all then display all item
+                if (category == "all") {
+                    displayItemMenu(menu)
                 }
-              })
-              // if category is equal to all then display all item
-              if(category=="all"){
-                displayItemMenu(menu)
-              }
-              // if category is not equal to all then show the filter arry
-              else{
-                displayItemMenu(CategoryMenu)
-              }
+                // if category is not equal to all then show the filter arry
+                else {
+                    displayItemMenu(CategoryMenu)
+                }
             })
         })
     }
@@ -131,9 +131,9 @@ function displayButton(menu){
 
 
 // display the item
-function displayItemMenu(menuItem){
-    let displayMenu = menuItem.map(menu =>{
-    return `
+function displayItemMenu(menuItem) {
+    let displayMenu = menuItem.map(menu => {
+        return `
     <div class="items">
     <div class="image">
         <img src=${menu.image}>
@@ -142,6 +142,7 @@ function displayItemMenu(menuItem){
     <div class="title">
         ${menu.title}
     </div>
+    <button class="cart" type="button" value="add to cart">add to cart</button>
     <div class="price">
         ${menu.price}
     </div>
@@ -153,14 +154,14 @@ function displayItemMenu(menuItem){
     `
     });
     // console.log(displayItem)
-    displayMenu=displayMenu.join("")
-    sectionCentre.innerHTML= displayMenu
+    displayMenu = displayMenu.join("")
+    sectionCentre.innerHTML = displayMenu
 }
 // scrol window on click                         
-function scroll(){
+function scroll() {
     window.scrollBy({
-        top:1000,
-        behavior:"smooth"
+        top: 1000,
+        behavior: "smooth"
     })
 }
-document.querySelector(".filterbtn").addEventListener("click",scroll)
+document.querySelector(".filterbtn").addEventListener("click", scroll)
